e616f755228d794fe06936fa5cab9ff6d88784a0
tamzi <tamrefrank@gmail.com>
2020-03-22 11:27:56 +0300

    string first example

---
+++ strings/strings.dart
@@ -0,0 +1,11 @@
+main(){
+
+  /*first way to write a string*/
+  String subject = 'biology';
+  String time = '5pm';
+
+  print('The ${subject} class starts at ${time}');
+  print(time);
+  print('biology');
+
+}
