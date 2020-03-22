7723f0a5c7e53c4639139310cacd5c4d75a31a6d
tamzi <tamrefrank@gmail.com>
2020-03-22 16:24:31 +0300

    added example

--- strings/strings.dart
+++ strings/strings.dart
@@ -1,9 +1,11 @@
 main(){
 
-  /*first way to write a string*/
+  /*ways to declare and print a string*/
   String subject = 'biology';
   String time = '5pm';
+  String teacherName = 'Mr.Jonah';
 
+  print('Hello ${teacherName}');
   print('The ${subject} class starts at ${time}');
   print(time);
   print('biology');
