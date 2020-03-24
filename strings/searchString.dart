d7efa3451e93a646e019b53034ad4314e5dea6dd
tamzi <tamrefrank@gmail.com>
2020-03-24 21:22:37 +0300

    search a string

---
+++ strings/searchString.dart
@@ -0,0 +1,15 @@
+main(){
+
+  /*search for a word in the string*/
+  String cicero = 'Cicero was born on 3 January 106 BC in Arpinum, a hill town 100 kilometers southeast of Rome.\nHe belonged to the tribus Cornelia.\nHis father was a well-to-do member of the equestrian order and possessed good connections in Rome\nHowever, being a semi-invalid, he could not enter public life and studied extensively to compensate.\nAlthough little is known about the mother to Cicero, Helvia, it was common for the wives of important Roman citizens to be responsible for the management of the household.';
+
+  print('${cicero}');
+  /*Important to note how case sentitive it is as per below examples*/
+      print(cicero.contains('Cicero'));
+      print(cicero.contains('cicero'));
+
+
+      /*Making it more human friendly*/
+  print('Is there "cicero" in the above paragraph?: ${cicero.contains('cicero')}');
+  print('Is there "cicero" in the above paragraph?: ${cicero.contains('Cicero')}');
+}
