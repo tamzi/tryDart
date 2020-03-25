6013aa569e7e03e91e4ec9e9b5b46d3e9c023698
tamzi <tamrefrank@gmail.com>
2020-03-25 10:55:17 +0300

    Added constantVariableString.dart

---
+++ strings/constantVariableString.dart
@@ -0,0 +1,14 @@
+main(){
+
+  String fName = 'Paulo';
+  const String sName = 'Cossie';
+
+  /*
+  sName = 'paul';
+  This will cause an error. try checking by uncommenting the code!
+   */
+
+  print(fName +' '+ sName);
+
+
+}
