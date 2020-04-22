5a29eb9f3b4d439b2ac979addbc0f86cb5de818b
tamzi <tamrefrank@gmail.com>
2020-04-22 23:53:23 +0300

    Added enum.dart

--- collections/enum.dart
+++ collections/enum.dart
@@ -1,8 +1,11 @@
 
 /*Declare an Enum outside a main function*/
 enum students {Joanna, Phill, Patricia, Frank,Diana}
+enum age {Twenty,Fifteen,Seven, Fourty, Thirty}
 
 main(){
   print(students.values);
   print(students.Diana);
+  print (students.Phill);
+  print(age.Twenty);
 }
